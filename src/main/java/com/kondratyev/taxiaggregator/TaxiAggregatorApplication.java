ed57afb0e97d43c3a8b04ccd52871c40dfa4353f
kondratyev-a <alex.a.kondratyev@gmail.com>
2021-08-12 11:15:17 +0700

    Initial commit

---
+++ src/main/java/com/kondratyev/taxiaggregator/TaxiAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.kondratyev.taxiaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kondratyev/taxiaggregator/TaxiAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kondratyev.taxiaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
